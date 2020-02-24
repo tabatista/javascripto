40fff0bc0c9e83ee5fb27ea9d85f1362367c8783
Thais Poentes <thais_poentes@hotmail.com>
2020-02-23 23:26:47 -0300

    esmaecer linha excluida

--- js/remover.js
+++ js/remover.js
@@ -4,8 +4,17 @@ let tabela = document.querySelector('#tabela-pacientes');
 
 tabela.addEventListener('dblclick', function (event) {
     let alvoEvento = event.target;
-    let paiAlvo = alvoEvento.parentNode;
-    //ao  inves de excluir o alvo apenas (td) ele exclui o pai consigo (tr);
-    paiAlvo.remove();
+    let paiAlvo = alvoEvento.parentNode; //delegacao de evento - event bubbling
+
+    //esmaecer a linha
+    paiAlvo.classList.add('fadeOut');
+
+    //para esperar a funcao acima funcionar
+    setTimeout(() => {
+        
+        //ao  inves de excluir o alvo apenas (td) ele exclui o pai consigo (tr);
+        paiAlvo.remove();
+    }, 700); //milisegundos para executar
+
 
 });
